d5d895c78f155994d534b06f793da6c1bf248d4e
JohannesTamerl <120123052+JohannesTamerl@users.noreply.github.com>
2024-03-15 14:12:35 +0100

    Create SingleLinkedList.py

---
+++ SingleLinkedList.py
@@ -0,0 +1,53 @@
+class Node:
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+
+    def __str__(self):
+        return str(self.data)
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        if not self.head:
+            self.head = Node(data)
+        else:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = Node(data)
+
+    def length(self):
+        count = 0
+        current = self.head
+        while current:
+            count += 1
+            current = current.next
+        return count
+
+    def __iter__(self):
+        current = self.head
+        while current:
+            yield current.data
+            current = current.next
+
+    def __str__(self):
+        result = []
+        current = self.head
+        while current:
+            result.append(str(current.data))
+            current = current.next
+        return ' -> '.join(result)
+
+
+if __name__ == '__main__':
+    ll = LinkedList()
+    ll.append(1)
+    ll.append(2)
+    ll.append(3)
+    for i in ll:
+        print(i)
+    print(ll.__str__()) 
